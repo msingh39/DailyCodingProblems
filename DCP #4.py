90d0b97efa746075e3609d803b29ef80c097f45c
msingh39 <msingh39@illinois.edu>
2020-01-23 11:45:34 -0600

    Update DCP #4.py

--- DCP #4.py
+++ DCP #4.py
@@ -5,27 +5,28 @@
 # For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
 # You can modify the input array in-place.
 
-#My Soltuion
+# My Soltuion
 
-#Function to get positive values from a list
+# Function to get positive values from a list
 def positive_array(num_list):
     num_list[:] = [i for i in num_list if i >= 0]
     return num_list
 
 #Function to find lowest positive number not in original list
 def lowest_positive_number(num_list):
-    #Call helper function
+    # Call helper function
     num_list = positive_array(num_list)
-    #Create a new list from lowest to highest value in original list
+    
+    # Create a new list from lowest to highest value in original list
     num_list_min_max = list(range(min(num_list),max(num_list) + 2))
 
-    #Loop through and check if value in the new list is in the original, if not return that value
+    # Loop through and check if value in the new list is in the original, if not return that value
     for i in range(len(num_list_min_max)):
         return [i for i in num_list_min_max if i not in num_list][0]
 
-#Test Case
-test_1 = [3,4,-1,1] #Should return 2
+# Test Case
+test_1 = [3,4,-1,1] # Should return 2
 print(lowest_positive_number(test_1))
 
-test_2 = [1,2,0] #shoudl return 3
+test_2 = [1,2,0] # Should return 3
 print(lowest_positive_number(test_2))
